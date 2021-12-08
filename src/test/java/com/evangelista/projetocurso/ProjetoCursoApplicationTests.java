ea9cfe4a151d6e69a93c15525df6ba4e69c6d768
Fabio Evangelista <fabioevangelista_@hotmail.com>
2021-12-08 17:51:21 -0300

    first commit

---
+++ src/test/java/com/evangelista/projetocurso/ProjetoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evangelista.projetocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
