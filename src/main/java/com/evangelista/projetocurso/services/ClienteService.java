b10865adb38dee249244a57fb9a211c40b166954
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-01-04 18:48:53 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/evangelista/projetocurso/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.evangelista.projetocurso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.evangelista.projetocurso.domain.Cliente;
+import com.evangelista.projetocurso.repositories.ClienteRepository;
+import com.evangelista.projetocurso.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " +
+																	id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
