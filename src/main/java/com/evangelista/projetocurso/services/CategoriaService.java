227fbb0813aa89243c1fe3efcbf146e0ed83f150
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-02-10 21:11:47 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/evangelista/projetocurso/services/CategoriaService.java
+++ src/main/java/com/evangelista/projetocurso/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.evangelista.projetocurso.domain.Categoria;
+import com.evangelista.projetocurso.dto.CategoriaDTO;
 import com.evangelista.projetocurso.repositories.CategoriaRepository;
 import com.evangelista.projetocurso.services.exceptions.DataIntegrityException;
 import com.evangelista.projetocurso.services.exceptions.ObjectNotFoundException;
@@ -57,6 +58,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
-		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
